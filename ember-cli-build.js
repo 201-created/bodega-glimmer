c0a285be2af398fb6a25b0401bcf181e967333e2
Matthew Beale <matt.beale@madhatted.com>
2017-07-06 08:19:25 -0700

    Service worker support

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,27 @@ const MergeTrees = require('broccoli-merge-trees');
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
     // Add options here
+    'asset-cache': {
+      include: [
+        'assets/**/*',
+        'app*.css',
+        'app*.js'
+      ],
+      manual: [
+        'vendor/flickity.pkgd.min.js'
+      ],
+      exclude: [
+        '/',
+        'index.html'
+      ]
+    },
+    'esw-cache-fallback': {
+      patterns: [
+        '/',
+        'https://api.shop-201.com/api/items'
+      ],
+      version: '3' // Changing the version will bust the cache
+    }
   });
 
   let flickity = new Funnel('node_modules/flickity/dist', {
