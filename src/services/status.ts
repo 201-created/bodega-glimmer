95bf167a28f9fc2816bce675cd0bf19fe19e238f
Cory <cory.forsyth@gmail.com>
2017-07-10 15:58:45 -0600

    remove unused stuff from status service

--- src/services/status.ts
+++ src/services/status.ts
@@ -3,10 +3,4 @@ import { tracked } from '@glimmer/component';
 
 export default class StatusService extends Service {
   @tracked errorMessage = null;
-  @tracked charge = null;
-  @tracked didCompletePayment = false;
-
-  didCompletePaymentWithCharge(charge) {
-    return new StatusService({errorMessage: null, charge, didCompletePayment: true});
-  }
 }
